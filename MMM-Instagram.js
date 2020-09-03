5b3501ccb2a72347c02d33fab4b94f192cb217f9
Mikael Östberg <Mikael.Ostberg@telenor.se>
2020-09-03 15:38:08 +0200

    Use basic display API instead of old removed API

--- MMM-Instagram.js
+++ MMM-Instagram.js
@@ -9,131 +9,158 @@
 
 Module.register('MMM-Instagram', {
 
-    defaults: {
-        apiUrl: 'https://api.instagram.com/v1/users/self/media/recent',
-        format: 'json',
-        lang: 'en-us',
-        id: '',
-        animationSpeed: 1000,
-        updateInterval: 60000, // 10 minutes
-        access_token: '',
-        count: 200,
-        randomize: false,
-        min_timestamp: 0,
-        instaMaxWidth: '100%',
-        instaMaxHeight: '100%',
-        loadingText: 'Loading...'
-    },
+	defaults: {
+	    apiUrl: "https://graph.instagram.com/me/media",
+	    format: "json",
+	    lang: "en-us",
+	    id: "",
+	    animationSpeed: 1000,
+	    updateInterval: 60000, // 10 minutes
+	    access_token: "",
+	    count: 200,
+	    randomize: false,
+	    min_timestamp: 0,
+	    instaMaxWidth: "100%",
+	    instaMaxHeight: "100%",
+	    loadingText: "Fetching ..."
+	},
 
-    // Define required scripts
-    getScripts: function() {
-        return ["moment.js"];
-    },
+	// Define required scripts
+	getScripts: function () {
+		return ["moment.js"];
+	},
 
-    // Define start sequence
-    start: function() {
-        Log.info('Starting module: ' + this.name);
-        this.data.classes = 'bright medium';
-        this.loaded = false;
-        this.images = [];
-        this.activeItem = 0;
-        this.apiUrls = this.getApiUrls();
-        this.sendSocketNotification("INSTAGRAM_GET", this.apiUrls);
-    },
+	// Define start sequence
+	start: function () {
+		Log.info("Starting module: " + this.name);
+		this.data.classes = "bright medium";
+		this.loaded = false;
+		this.media = [];
+		this.activeItem = 0;
+		this.apiUrls = this.getApiUrls();
+		this.sendSocketNotification("INSTAGRAM_GET", this.apiUrls);
+	},
 
-    getStyles: function() {
-        return ['instagram.css', 'font-awesome.css'];
-    },
+	getStyles: function () {
+		Log.info("styles");
+		return ["instagram.css", "font-awesome.css"];
+	},
 
-    // Override the dom generator
-    getDom: function() {
-        var wrapper = document.createElement("div");
-        var imageDisplay = document.createElement('div'); //support for config.changeColor
+	// Override the dom generator
+	getDom: function () {
+		var wrapper = document.createElement("div");
+		var mediaDisplay = document.createElement("div"); //support for config.changeColor
 
-        if (!this.loaded) {
-            wrapper.innerHTML = this.config.loadingText;
-            return wrapper;
-        }
+		if (!this.loaded) {
+			wrapper.innerHTML = this.config.loadingText;
+			return wrapper;
+		}
 
-        // set the first item in the list...
-        if (this.activeItem >= this.images.length) {
-            this.activeItem = 0;
-        }
+		// set the first item in the list...
+		if (this.activeItem >= this.media.length) {
+			this.activeItem = 0;
+		}
 
-        var tempImage = this.images[this.activeItem];
+		var mediaItem = this.media[this.activeItem];
+		Log.info("Creating video element for type " + mediaItem.media_type + " for media_url: " + mediaItem.media_url)
 
-        var imageWrapper = document.createElement("img");
-  	    imageWrapper.src = tempImage.photolink;
-  	    imageWrapper.id = "MMM-Instagram-image";
-  	    imageWrapper.style.maxWidth = this.config.instaMaxWidth;
-  	    imageWrapper.style.maxHeight = this.config.instaMaxHeight;
-  	    imageDisplay.appendChild(imageWrapper);
+		if (mediaItem.media_type === "IMAGE" || mediaItem.media_type === "CAROUSEL_ALBUM") {
+			var imageWrapper = this.getImageWrapper(mediaItem);
+			mediaDisplay.appendChild(imageWrapper);
+		} else if (mediaItem.media_type === "VIDEO") {
+			var videoWrapper = this.getVideoWrapper(mediaItem);
+			mediaDisplay.appendChild(videoWrapper);
+		}
+		wrapper.appendChild(mediaDisplay);
+		return wrapper;
+	},
 
-	      wrapper.appendChild(imageDisplay);
+	getImageWrapper: function (image) {
+		Log.info("styles");
+		var img = document.createElement("img");
+		img.src = image.media_url;
+		img.id = "MMM-Instagram-image";
+		img.setAttribute("width", "300");
+		img.style.maxWidth = this.config.instaMaxWidth;
+		img.style.maxHeight = this.config.instaMaxHeight;
+		return img;
+	},
 
-        return wrapper;
-    },
+	getVideoWrapper: function (video) {
+		var videoWrapper = document.createElement("video");
+		videoWrapper.setAttribute("autoplay", "");
+		videoWrapper.setAttribute("muted", "");
+		videoWrapper.setAttribute("loop", "");
+		videoWrapper.setAttribute("width", "300");
+		videoWrapper.style.maxWidth = this.config.instaMaxWidth;
+		videoWrapper.style.maxHeight = this.config.instaMaxHeight;
+		var sourceWrapper = document.createElement("source");
+		sourceWrapper.src = video.media_url;
+		sourceWrapper.id = "MMM-Instagram-image";
+		sourceWrapper.type = "video/mp4"
+		videoWrapper.appendChild(sourceWrapper);
+		return videoWrapper;
+	},
 
-    /* scheduleUpdateInterval()
-     * Schedule visual update.
-     */
-    scheduleUpdateInterval: function() {
-        var self = this;
+	/* scheduleUpdateInterval()
+	 * Schedule visual update.
+	 */
+	scheduleUpdateInterval: function () {
+	    var self = this;
 
-        Log.info("Scheduled update interval set up...");
-        self.updateDom(self.config.animationSpeed);
+	    Log.info("Scheduled update interval set up...");
+	    self.updateDom(self.config.animationSpeed);
 
-        setInterval(function() {
-            Log.info("incrementing the activeItem and refreshing");
-            self.activeItem++;
-            self.updateDom(self.config.animationSpeed);
-        }, this.config.updateInterval);
-    },
+	    setInterval(function () {
+	        Log.info("incrementing the activeItem and refreshing");
+	        self.activeItem++;
+	        self.updateDom(self.config.animationSpeed);
+	    }, this.config.updateInterval);
+	},
 
-    /*
+	/*
      * getApiUrls()
      * returns the query strings required for the request to instagram to get the
      * photo streams of the users requested
      */
-    getApiUrls: function() {
-        var tokens = this.config.access_token || this.config.access_tokens; //default for backwards compatibility
-        if(!Array.isArray(tokens)){
-          tokens = [tokens];
-        }
-        return tokens.map(token => {
-          var url = this.config.apiUrl + '?';
-          url += 'count=' + this.config.count;
-          url += '&min_timestamp=' + this.config.min_timestamp;
-          url += '&access_token=' + token;
-          return url;
-        });
-    },
+	getApiUrls: function() {
+		var tokens = this.config.access_token || this.config.access_tokens; //default for backwards compatibility
+		if(!Array.isArray(tokens)){
+			tokens = [tokens];
+		}
+		return tokens.map(token => {
+			var url = this.config.apiUrl;
+			url += "?fields=caption,media_url,media_type,id";
+			url += "&limit=200";
+			url += "&access_token=" + token;
+			return url;
+		});
+	},
 
-    shuffle: function(array) {
-      for (var i = array.length - 1; i > 0; i--) {
-          var j = Math.floor(Math.random() * (i + 1));
-          var temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-      }
-      return array;
-    },
+	shuffle: function (array) {
+	    for (var i = array.length - 1; i > 0; i--) {
+	        var j = Math.floor(Math.random() * (i + 1));
+	        var temp = array[i];
+	        array[i] = array[j];
+	        array[j] = temp;
+	    }
+	    return array;
+	},
+	// override socketNotificationReceived
+	socketNotificationReceived: function (notification, payload) {
+	    //Log.info('socketNotificationReceived: ' + notification);
+	    if (notification === "INSTAGRAM_IMAGE_LIST") {
+	        this.media = this.config.randomize ? this.shuffle(payload) : payload;
 
-    // override socketNotificationReceived
-    socketNotificationReceived: function(notification, payload) {
-        //Log.info('socketNotificationReceived: ' + notification);
-        if (notification === 'INSTAGRAM_IMAGE_LIST')
-        {
-            this.images = this.config.randomize ? this.shuffle(payload) : payload;
+	        // we want to update the dom the first time and then schedule next updates
+	        if (!this.loaded) {
+	            this.updateDom(1000);
+	            this.scheduleUpdateInterval();
+	        }
 
-            // we want to update the dom the first time and then schedule next updates
-            if (!this.loaded) {
-              this.updateDom(1000);
-              this.scheduleUpdateInterval();
-            }
+	        this.loaded = true;
+	    }
+	}
 
-            this.loaded = true;
-        }
-    }
 
 });
--- node_helper.js
+++ node_helper.js
@@ -5,48 +5,49 @@
  * MIT Licensed.
  */
 
- var NodeHelper = require("node_helper");
- var rp = require('request-promise-native');
+var NodeHelper = require("node_helper");
+var rp = require("request-promise-native");
 
- module.exports = NodeHelper.create({
+module.exports = NodeHelper.create({
 
-    start: function() {
-        console.log("Starting node_helper for module [" + this.name + "]");
-    },
+	start: function() {
+		console.log("Starting node_helper for module [" + this.name + "]");
+	},
 
-    socketNotificationReceived: function(notification, apiUrls){
-        if (notification === 'INSTAGRAM_GET') {
-            this.fetchImages(apiUrls);
-        }
-    },
+	socketNotificationReceived: function(notification, apiUrls){
+		if (notification === "INSTAGRAM_GET") {
+			this.fetchImages(apiUrls);
+		}
+	},
 
-    fetchImages: function(apiUrls) {
-      var self = this;
-      var promises = apiUrls.map(url => {
-        return rp({
-          uri: url,
-          method: 'GET',
-          json: true
-        }).then(items => {
-          return items.data.map(item => {
-            return {
-              "type": item.type,
-              "photolink": item.images.low_resolution.url
-            };
-          });
-        }).catch(error => {
-          console.log("Error requesting " + url + ": ", error);
-        });
-      });
-      Promise.all(promises)
-        .then(results => {
-          self.sendSocketNotification('INSTAGRAM_IMAGE_LIST',
-            results.reduce((acc, item) => {
-              return acc.concat(item);
-            }, [])
-          );
-        }).catch(error => {
-          console.log("Error: ", error);
-        });
-    }
- });
+	fetchImages: function(apiUrls) {
+		var self = this;
+		var promises = apiUrls.map(url => {
+			return rp({
+				uri: url,
+				method: "GET",
+				json: true
+			}).then(items => {
+				var media = items.data.map(item => {
+					return {
+						"media_type": item.media_type,
+						"media_url": item.media_url
+					};
+				});
+				return media;
+			}).catch(error => {
+				console.log("Error requesting " + url + ": ", error);
+			});
+		});
+		Promise.all(promises)
+			.then(results => {
+				self.sendSocketNotification("INSTAGRAM_IMAGE_LIST",
+					results.reduce((acc, item) => {
+						return acc.concat(item);
+					}, [])
+				);
+			}).catch(error => {
+				console.log("Error: ", error);
+			});
+	}
+});
